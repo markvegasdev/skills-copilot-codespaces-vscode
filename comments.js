c29721ce523abb5e1126274cfc83a4bf8732c2c1
Markvegasdev <84959368+markvegasdev@users.noreply.github.com>
2023-11-27 14:22:36 +0000

    Fix errors with the last commit

--- comments.js
+++ comments.js
@@ -1,21 +1,13 @@
-// Create a web server
-// Use the file system to read a file
-// Send the contents of the file to the client
-// Set the content type of the response
-// Set the status code of the response
+// create a web server
+// run this file using node comments.js
+// then go to your browser and type localhost:3000
+// then you will see "Hello World"
 
-// Create a web server
 const http = require('http');
-const server = http.createServer((request, response) => {
-  // Use the file system to read a file
-  const fs = require('fs');
-  fs.readFile('index.html', 'utf8', (errors, contents) => {
-    // Send the contents of the file to the client
-    response.writeHead(200, {'Content-Type': 'text/html'}); // Set the content type of the response
-    response.write(contents); // Send the response body
-    response.end(); // Finished!
-  });
+
+const server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
 });
 
-server.listen(6789); // Set the status code of the response
-console.log("Running in localhost at port 6789");
+server.listen(3000, '');
